3d90d1a3032248dfbe94eac74cca9375ea61a592
fipo29 <fidel@lacroixrouge-brest.fr>
2018-03-29 17:18:15 +0200

    MAJ

--- Programmes-unitaires/Arduino/Anemometre/Anemometre.ino
+++ Programmes-unitaires/Arduino/Anemometre/Anemometre.ino
@@ -1,6 +1,6 @@
 int Vitesse;
 int i = 0;
-int broche=2; // capteur sur la broche numérique 2
+int broche = 2; // capteur sur la broche numérique 2
 
 void setup() {
 
@@ -11,9 +11,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Vitesse = LectureVitVent(broche);
-  Serial.print("Vitesse du vent en m/s: ");
-  Serial.println(Vitesse);
-
+  if (Vitesse > 0) {
+    Serial.print("Vitesse du vent en m/s: ");
+    Serial.println(Vitesse);
+  }
 }
 
 float LectureVitVent(int Broche) {
@@ -22,10 +23,11 @@ float LectureVitVent(int Broche) {
   float perimetre = 2 * 3.14 * 0.06; //rayon = 0.06 m
   float tempHaut = pulseIn(Broche, LOW);
   float tempBas = pulseIn(Broche, HIGH);
-  float duree = (tempHaut + tempBas) * 0.000001; //duree en s
+  float duree = (tempHaut + tempBas); //duree en us
 
   if (tempHaut > 0 & tempBas > 0) {
     vitesse = (perimetre / duree);
+    //vitesse=vitesse*0.000001;
     return vitesse;
   }
   else return 0;
